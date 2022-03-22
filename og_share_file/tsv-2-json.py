d0e0fd2d0e1af41327f1e6ac2c1844b4eee4c0af
markmak233 <90290156+markmak233@users.noreply.github.com>
2022-03-21 21:52:55 -0600

    p1a

---
+++ og_share_file/tsv-2-json.py
@@ -0,0 +1,56 @@
+import json,sys
+  
+def tsv2json(input_file,output_file):
+    arr = []
+    try:
+        file = open(input_file, 'r')
+        a = file.readline()
+    except:
+        print("failed to open {}.".format(input_file))
+      
+    # The first line consist of headings of the record 
+    # so we will store it in an array and move to 
+    # next line in input_file.
+    titles = [t.strip() for t in a.split('\t')]
+    for line in file:
+        d = {}
+        for t, f in zip(titles, line.split('\t')):
+            # replace some of the elements in the list
+            f=f.strip().replace('\\N','NULL')
+            f=f.strip().replace('[','')
+            f=f.strip().replace(']','')
+            f=f.strip().replace('"','')
+            if ',' in f and t in ['primaryProfession','knownForTitles','genres','characters']:
+                d[t] = [c.strip() for c in f.split(',')]
+            else:
+                # Convert each row into dictionary with keys as titles
+                d[t] = f.strip()
+              
+        # we will use strip to remove '\n'.
+        arr.append(d)
+        # we will append all the individual dictionaires into list 
+        # and dump into file.
+    try:
+        with open(output_file, 'w', encoding='utf-8') as output_file:
+            output_file.write(json.dumps(arr, indent=4))
+    except Exception as e:
+        print(e)
+    print("output_file",output_file)
+  
+# Driver Code
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print(" transcoding:\n name.basics.tsv \n title.basics.tsv\n title.principals.tsv\n title.ratings.tsv ")
+        list_of_file=['name.basics.tsv','title.basics.tsv','title.principals.tsv','title.ratings.tsv']
+    else:
+        # python3 transfer_off.py name.basics.tsv title.basics.tsv title.principals.tsv title.ratings.tsv
+        list_of_file=[]
+        print(" transcoding:")
+        for i in range(1,len(sys.argv)):
+            list_of_file.append(sys.argv[i])
+            print(" {} ".format(sys.argv[i]))
+    print('-'*12)
+    for i in range(len(list_of_file)):
+        input_filename = list_of_file[i]
+        output_filename = input_filename[:-4]+'.json'
+        tsv2json(input_filename,output_filename)
