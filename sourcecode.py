a195a07a9b6a8ce560ee5efa506fd2b6b417d05e
BlaC-t <98289710+BlaC-t@users.noreply.github.com>
2022-03-24 17:52:36 -0600

    Add files via upload

---
+++ sourcecode.py
@@ -0,0 +1,33 @@
+from pymongo import MongoClient
+import loaddb
+import sys
+
+def search_title():
+    ...
+
+
+def search_genres():
+    ...
+
+
+def search_caster():
+    ...
+
+
+def add_movie():
+    ...
+
+
+def add_castre():
+    ...
+
+
+def menu():
+    ...
+
+def main():
+    loaddb.main()
+
+
+if "__name__" == "__main__":
+    main()
